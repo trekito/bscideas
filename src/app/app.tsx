808a552b84e64eaedb71521367fd3daa1ed7ae17
Tomas <t.kotrla@centrum.cz>
2021-03-10 11:09:24 +0100

    menu style

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,14 @@
 import './app.css';
+import './css/tomas-kotrla.css';
 
 export const App = () => {
   return (
     <nav>
-      <ul>
-        <li>Dashboard</li>
-        <li>Account</li>
-        <li>Payments</li>
-        <li>Settings</li>
+      <ul className="menu">
+        <li className="menu__item">Dashboard</li>
+        <li className="menu__item">Account</li>
+        <li className="menu__item">Payments</li>
+        <li className="menu__item">Settings</li>
       </ul>
     </nav>
   );
